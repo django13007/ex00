c997160d0529c0222a6c8f5fe67e3f15b45aad4c
django13007 <164808086+django13007@users.noreply.github.com>
2024-03-30 15:22:01 -0500

    modulo_1

--- ex05/ft_print_comb.c
+++ ex05/ft_print_comb.c
@@ -17,6 +17,10 @@ while (n1 <= 57)
             write(1, &n1 ,1);
             write(1, &n2 ,1);
             write(1, &n3 ,1); 
+            if (n1==55 && n2==56 && n3==57)
+            {
+                return;
+            }
             write(1, ",",1);
             write(1, " " ,1);
 
